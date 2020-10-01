23be2c2ee0831c53d4284e2c679b1731416a569f
ratedstark <espinito_manda@hotmail.com>
2020-09-30 22:06:03 -0500

    docs added

---
+++ docs/precache-manifest.18535a225ddca00c6b7e5759ef1816d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b94137d3da96e84e955021fa250e63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d17dba980b096d037385",
+    "url": "/static/css/main.3039e480.chunk.css"
+  },
+  {
+    "revision": "05792a1f53564e0adfa8",
+    "url": "/static/js/2.879a03b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.879a03b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17dba980b096d037385",
+    "url": "/static/js/main.085e4fbf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
